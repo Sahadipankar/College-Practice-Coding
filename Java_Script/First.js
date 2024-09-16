8710977b9bd4fd4f3a8ccd37dede5064a7cecdb3
Sahadipankar <sahadepankar@gmail.com>
2024-09-17 01:13:18 +0530

    JavaScript file modified

--- Java_Script/First.js
+++ Java_Script/First.js
@@ -18,3 +18,41 @@ console.log(typeof amazonProduct["rating"]);
 console.log(typeof amazonProduct["isDeal"]);
 console.log(typeof amazonProduct["price"]);
 console.log(typeof amazonProduct["offer"]);
+
+// let num = prompt("Enter a number.");
+// if (num % 5 == 0) console.log(num, "is a multiple of 5.");
+// else console.log(num, "is not a multiple of 5.");
+
+let data = document.querySelector("h2");
+console.dir(data);
+console.log(data);
+
+console.dir(data.innerText);
+console.log(data.innerText);
+console.log(data.textContent); 
+console.log(data.innerHTML);
+
+let click = document.querySelector("button");
+click.onclick = function () {
+    console.log("Button clicked.");
+    data.innerHTML = "This is the new Inner HTML";
+};
+
+
+const person = {
+    name: "John",
+    age: 30,
+    address: {
+        city: "New York",
+        zip: 10001
+    }
+};
+
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.address);
+console.log(person.address.city);
+console.log(person.address.zip);
+
+console.dir(person)
